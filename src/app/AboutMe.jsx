de28498182673dcd29f05781f52ba9b9a2417db2
ShimaMichael <99474784+ShimaMichael@users.noreply.github.com>
2024-09-20 08:52:46 +0100

    added projects.jsx (#1)

--- src/app/AboutMe.jsx
+++ src/app/AboutMe.jsx
@@ -1,8 +1,15 @@
+import sample_image from 'next/image';
+
 export default function AboutMe() {
     return(
         <div className="AboutMe">
-            <h1>About Me</h1>
-            <p>software engineer at the university of hertfordshire looking to make bank.</p>
+            <div className="aboutMe_content">
+                <h1>About Me</h1>
+                <p>software engineer at the university of hertfordshire looking to make bank.</p>
+            </div>
+            <div className="image">
+                <img class="image-icon" alt="image" src=""></img>
+            </div>
         </div>
     );
 }
